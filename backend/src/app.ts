64cc06220c028fe4d0746ad8f60bca10c596fcbf
tiwariParth <243mjparth@gmail.com>
2024-12-05 22:33:46 +0530

    add Express server setup with basic routing and necessary dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
